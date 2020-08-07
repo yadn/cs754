e5132f688bb7c1a3d81aeeb670d9515c2ac1cd42
YADNYESH SANTOSH PATIL <yadny@cse.iitb.ac.in>
2020-08-07 16:27:31 +0530

    added code and report

--- code/filteredBackProjection2D.m
+++
@@ -1,6 +0,0 @@
-function fbp = filteredBackProjection2D(y,numAngles,idx1,dim)
-
-proj = y;
-%proj = reshape(y,[size(y,1)/numAngles numAngles]);
-fbp = iradon(proj,idx1,'linear','cosine');
-fbp = fbp(2:2+dim(1)-1,2:2+dim(2)-1);
